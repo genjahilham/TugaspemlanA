e3c24cad683d4fc30ebe1570e16b54d581922239
genjahilham <genjahilham1@gmail.com>
2020-09-23 08:38:34 +0700

    pertama commit

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main()
+
+ {
+ 	
+ 	printf("\tBIODATA PRIBADI\n\n");
+ 	printf("Nama                  :Genjah Ilham Zamzami\n");
+ 	printf("NPM                   :19081010147\n");
+ 	printf("Kelas                 :Pemrogaman lanjut A\n");
+ 	printf("Jurusan               :Teknik Informatika\n");
+ 	printf("Tempat Tanggal lahir  :Surabaya,01 juni 2001\n");
+ 	printf("Hobi                  :Bermain Game\n");
+ 	printf("Alamat                :Jl.Kedondong kidul 3 no 29");
+ 	
+ 	getch();
+ 	
+	return 0;
+}
